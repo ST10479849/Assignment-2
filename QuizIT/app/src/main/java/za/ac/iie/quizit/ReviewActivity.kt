12ff7db7956d1fe05ce34b4eb02cf0de974c6997
ST10479849 <ST10479849@imconnect.edu.za>
2025-05-19 13:19:14 +0200

    fourth commit
    reviewing of answers

--- QuizIT/app/src/main/java/za/ac/iie/quizit/ReviewActivity.kt
+++ QuizIT/app/src/main/java/za/ac/iie/quizit/ReviewActivity.kt
@@ -1,19 +1,37 @@
 package za.ac.iie.quizit
 
+import android.annotation.SuppressLint
 import android.os.Bundle
+import android.widget.LinearLayout
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.setPadding
+import za.ac.iie.quizit.R.id.txtAnswerReview
 
 class ReviewActivity : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_review)
 
+     val txtAnswerReview = findViewById<LinearLayout>(R.id.txtAnswerReview)
+     val quiz = intent.getStringArrayExtra("Quiz")
+     val answers = intent.getBooleanArrayExtra("Answers")
 
-
+        if( quiz != null && answers != null ) {
+            for (i in quiz.indices) {
+                val answersView = TextView(this)
+                answersView.text =
+                    "${i + 1}. ${quiz[i]}\nAnswer: ${if (answers[i]) "True" else "False"}"
+                answersView.textSize = 16f
+                answersView.setPadding(0, 24, 0, 24)
+                txtAnswerReview.addView(answersView)
+            }
+        }
 
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
