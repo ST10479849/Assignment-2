0850a5ce7c50db23bdfff9f8ada20f864f9273d0
ST10479849 <ST10479849@imconnect.edu.za>
2025-05-21 15:21:20 +0200

    updated
    deleted extra view

---
+++ QuizIT/app/src/main/java/za/ac/iie/quizit/ReviewActivity.kt
@@ -0,0 +1,37 @@
+package za.ac.iie.quizit
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+
+class ReviewActivity : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId", "SuspiciousIndentation", "SetTextI18n")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_review)
+
+
+        // shows all the correct answers
+
+
+
+
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
+
+
+
+
